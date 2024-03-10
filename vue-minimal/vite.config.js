e38d9fb009755cf8f8fec62f6c5386d1f726eb81
Erik Mattheis <erik@mattheis.org>
2024-03-10 00:47:49 -0600

    Try vike-vue

--- vue-minimal/vite.config.js
+++ vue-minimal/vite.config.js
@@ -1,9 +1,21 @@
 import vue from '@vitejs/plugin-vue'
 import vike from 'vike/plugin'
+
 import { fileURLToPath, URL } from 'node:url'
 
+/* use  
+    "@vue/compiler-sfc": "^3.2.47",
+    "@vue/server-renderer": "^3.2.47",
+    "vike": "0.4.165",
+    "vite": "^5.0.10",
+    "vue": "^3.2.47",
+    "vuetify": "^3.5.8" */
+import vuetify from 'vuetify'
+
+
+
 export default {
-  plugins: [vue(), vike()],
+  plugins: [vue(), vuetify(), vike()],
   resolve: {
     alias: {
       '~': fileURLToPath(new URL('./', import.meta.url))
